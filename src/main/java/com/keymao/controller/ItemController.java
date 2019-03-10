df2e9f82ae42f9117eaeeffadfd96ab010c3d904
DaMingMing <1044096131@qq.com>
2019-03-11 00:59:09 +0800

    新增商品控制器以及商品服务类

---
+++ src/main/java/com/keymao/controller/ItemController.java
@@ -0,0 +1,47 @@
+package com.keymao.controller;
+
+import com.keymao.common.pojo.EasyUIDataGridResult;
+import com.keymao.common.utils.E3Result;
+import com.keymao.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.keymao.pojo.TbItem;
+
+/**
+ * 商品管理Controller
+ */
+@Controller
+public class ItemController {
+
+	@Autowired
+	private ItemService itemService;
+
+	@RequestMapping("/item/{itemId}")
+	@ResponseBody
+	private TbItem getItemById(@PathVariable Long itemId) {
+		TbItem tbItem = itemService.getItemById(itemId);
+		return tbItem;
+		//return null;
+	}
+
+	@RequestMapping("/item/list")
+	@ResponseBody
+	public EasyUIDataGridResult getItemList(Integer page, Integer rows) {
+		EasyUIDataGridResult result = itemService.getItemList(page, rows);
+		System.out.println("control结果list=" + result.getRows().size());
+		return result;
+	}
+
+	@RequestMapping(value = "/item/save",method = RequestMethod.POST)
+	@ResponseBody
+	public E3Result saveItem(TbItem item, String desc) {
+		E3Result result = itemService.addItem(item, desc);
+		return result;
+	}
+
+}
